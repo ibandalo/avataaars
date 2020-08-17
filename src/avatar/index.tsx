9ac4be22fd177459d80383181bee3467d5c891c5
Ivan Bandalo <ivan@kayak.com>
2020-08-17 22:28:44 +0200

    missed paths

--- src/avatar/index.tsx
+++ src/avatar/index.tsx
@@ -38,11 +38,11 @@ export default class Avatar extends React.Component<Props> {
           <circle id={`${uniqueId}_path-1`} cx='120' cy='120' r='120' />
           <path
             d='M12,160 C12,226.27417 65.72583,280 132,280 C198.27417,280 252,226.27417 252,160 L264,160 L264,-1.42108547e-14 L-3.19744231e-14,-1.42108547e-14 L-3.19744231e-14,160 L12,160 Z'
-            id='path-3'
+            id={`${uniqueId}_path-3`}
           />
           <path
             d='M124,144.610951 L124,163 L128,163 L128,163 C167.764502,163 200,195.235498 200,235 L200,244 L0,244 L0,235 C-4.86974701e-15,195.235498 32.235498,163 72,163 L72,163 L76,163 L76,144.610951 C58.7626345,136.422372 46.3722246,119.687011 44.3051388,99.8812385 C38.4803105,99.0577866 34,94.0521096 34,88 L34,74 C34,68.0540074 38.3245733,63.1180731 44,62.1659169 L44,56 L44,56 C44,25.072054 69.072054,5.68137151e-15 100,0 L100,0 L100,0 C130.927946,-5.68137151e-15 156,25.072054 156,56 L156,62.1659169 C161.675427,63.1180731 166,68.0540074 166,74 L166,88 C166,94.0521096 161.51969,99.0577866 155.694861,99.8812385 C153.627775,119.687011 141.237365,136.422372 124,144.610951 Z'
-            id='path-5'
+            id={`${uniqueId}_path-5`}
           />
         </defs>
         <g
@@ -67,7 +67,7 @@ export default class Avatar extends React.Component<Props> {
                   <use
                     id='Circle-Background'
                     fill='#E6E6E6'
-                    xlinkHref='#path-1'
+                    xlinkHref={`#${uniqueId}_path-1`}
                   />
                   <g
                     id={`${uniqueId}_Color/Palette/Blue-01`}
@@ -82,13 +82,13 @@ export default class Avatar extends React.Component<Props> {
                   <use xlinkHref={`#${uniqueId}_path-3`} />
                 </mask>
               ) : null}
-              <g id='Mask' />
+              <g id={`${uniqueId}_Mask`} />
               <g
                 id='Avataaar'
                 strokeWidth='1'
                 fillRule='evenodd'
                 mask={`url(#${uniqueId}_mask-4)`}>
-                <g id='Body' transform='translate(32.000000, 36.000000)'>
+                <g id={`${uniqueId}_Body`} transform='translate(32.000000, 36.000000)'>
                   <mask id={`${uniqueId}_mask-6`} fill='white'>
                     <use xlinkHref={`#${uniqueId}_path-5`} />
                   </mask>
